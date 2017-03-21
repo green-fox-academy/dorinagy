90b05343927c7625e5d0fb921d0754f74e80baf6
Nagy Dora <dorinagy06@gmail.com>
2017-03-21 12:21:52 +0100

    add today's first exercises, add gitignore

---
+++ Week2/Day2/src/HelloMe.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Nagy Dóra on 2017.03.21..
+ */
+public class HelloMe {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dori!");
+    }
+}
---
+++ Week2/Day2/src/HelloOthers.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nagy Dóra on 2017.03.21..
+ */
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, George!");
+        System.out.println("Hello, Nori!");
+        System.out.println("Hello, Norbi!");
+    }
+}
