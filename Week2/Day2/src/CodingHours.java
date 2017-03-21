3d310b05822a4ab6ff1710d9a83131d5e0dfeccf
Nagy Dora <dorinagy06@gmail.com>
2017-03-21 12:24:49 +0100

    add exercises 04, 05, 06

---
+++ Week2/Day2/src/CodingHours.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nagy Dóra on 2017.03.21..
+ */
+public class CodingHours {
+    public static void main(String[] args) {
+        float a = ((17 * 5) * 6);
+        float b = (17 * 52);
+        float c = 100;
+        System.out.println(a);
+        System.out.println(a / b * c + "%");
+    }
+}
---
+++ Week2/Day2/src/IntroduceYourself.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nagy Dóra on 2017.03.21..
+ */
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Dora Nagy");
+        System.out.println(20);
+        System.out.println(1.60);
+    }
+}
