05ece7fab95349d0bb6c9d6e98b88c48bc62731b
Nagy Dora <dorinagy06@gmail.com>
2017-03-21 22:14:31 +0100

    add exercises 23-24, loops

---
+++ Week2/Day2/src/EvenNumbers.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nagy Dóra on 2017.03.21..
+ */
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 500; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ Week2/Day2/src/ExamLine.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nagy Dóra on 2017.03.21..
+ */
+public class ExamLine {
+    public static void main (String[] args) {
+        for (int a = 0; a < 100; a++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
