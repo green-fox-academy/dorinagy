a2f4802ccdc35d3c789be749d897e6116a8ef4a9
Nagy Dora <dorinagy06@gmail.com>
2017-03-21 22:13:23 +0100

    add if conditions to exercise 22

--- Week2/Day2/src/VariableMutation2.java
+++ Week2/Day2/src/VariableMutation2.java
@@ -11,25 +11,27 @@ public class VariableMutation2 {
         System.out.println(out);
 
         int b = 13;
-        String out2 = "";
-        if (10 < b && b>20) {
-            out2 = "Sweet!";
+        String out2 = " ";
+        if (10 < b && b < 20) {
+            out2 = "Sweet";
+            System.out.println(out2);
         }
         if (b < 10) {
-            out2 = "More!"
+            out2 = "More!";
+            System.out.println(out2);
         }
         if (b > 20) {
-            out2 = "Less!"
+            out2 = "Less!";
+            System.out.println(out2);
         }
-        System.out.println(out2);
 
         int c = 123;
         int credits = 100;
-        boolean isBonus = False;
-        if ((credits >= 50) && (isBonus == False)) {
+        boolean isBonus = false;
+        if ((credits >= 50) && (isBonus == false)) {
             c = c - 1;
         }
-        if (isBonus == True) {
+        if (isBonus == true) {
             c = c;
         }
         System.out.println(c);
@@ -37,17 +39,17 @@ public class VariableMutation2 {
 
         int d = 8;
         int time = 120;
-        String out3 = "";
+        String out3 = " ";
         if ((d % 4 == 0) && (time <= 200)){
-            out3 = "check";
+            out3 = "Check";
+            System.out.println(out3);
         }
         if (time > 200) {
             out3 = "Time out";
+            System.out.println(out3);
         } else {
             out3 = "Run Forest Run!";
+            System.out.println(out3);
         }
-        
-        System.out.println(out3);
     }
-}
 }
