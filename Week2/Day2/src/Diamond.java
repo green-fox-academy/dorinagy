66308423fd808796c4fa4f819c61b331620d62e9
Nagy Dora <dorinagy06@gmail.com>
2017-03-22 10:20:09 +0100

    add exercise 29, draw diamond

---
+++ Week2/Day2/src/Diamond.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Nagy Dóra on 2017.03.22..
+ */
+public class Diamond {
+  public static void main(String[] args) {
+    int number = 7;
+    for (int i = 1; i < number; i += 2) {
+      for (int j = 0; j < 9 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+
+      System.out.print("\n");
+    }
+
+    for (int i = 7; i > 0; i -= 2) {
+      for (int j = 0; j < 9 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+
+      System.out.print("\n");
+    }
+  }
+}
