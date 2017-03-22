13bb7e4cb0d76efde56da6e780138d1d287babf5
Nagy Dora <dorinagy06@gmail.com>
2017-03-22 15:17:42 +0100

    add exercises 7-10, use arrays

---
+++ Week2/Day3/src/CompareLength.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Nagy Dóra on 2017.03.22..
+ */
+public class CompareLength {
+  public static void main(String[] args) {
+    int[] p1 = {1, 2, 3};
+    int[] p2 = {4, 5};
+    if (p1.length < p2.length) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+  }
+}
---
+++ Week2/Day3/src/Third.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Nagy Dóra on 2017.03.22..
+ */
+public class Third {
+  public static void main(String[] args) {
+    int[] q = {4, 5, 6, 7};
+    System.out.println(q[2]);
+  }
+}
