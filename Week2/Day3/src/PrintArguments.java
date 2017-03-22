dd817104bd65b4429127e6efe271e9dd0a7f831e
Nagy Dora <dorinagy06@gmail.com>
2017-03-22 13:10:45 +0100

    use function with multiple parameters in exercise 6

--- Week2/Day3/src/PrintArguments.java
+++ Week2/Day3/src/PrintArguments.java
@@ -3,6 +3,14 @@
  */
 public class PrintArguments {
   public static void main(String[] args) {
-    
+    String text1 = "text one";
+    String text2 = "text two";
+    String text3 = "text three";
+    printer(text1, text2, text3);
+  }
+  public static void printer(String... print) {
+    for(String i : print){
+      System.out.println(i + " ");
+    }
   }
 }
