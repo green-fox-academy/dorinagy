fe466aaf498ebe6441e995a1f7cbce58adcbddd9
Nagy Dora <dorinagy06@gmail.com>
2017-03-23 11:45:22 +0100

    add simple replace exercise

---
+++ Week2/Day4/src/SimpleReplace.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nagy Dóra on 2017.03.23..
+ */
+public class SimpleReplace {
+  public static void main(String... args) {
+    String example = "In a dishwasher far far away";
+    String replaced = example.replace("dishwasher", "galaxy");
+    System.out.println(replaced);
+  }
+}
