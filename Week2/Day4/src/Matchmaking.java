cbd3dd183919ae086a410c2ccbf97b4c509c5fa3
Nagy Dora <dorinagy06@gmail.com>
2017-03-23 16:26:56 +0100

    correct matchmaking exercise, use two for loops

--- Week2/Day4/src/Matchmaking.java
+++ Week2/Day4/src/Matchmaking.java
@@ -5,23 +5,22 @@ import java.util.Arrays;
  * Created by Nagy Dóra on 2017.03.23..
  */
 public class Matchmaking {
-  public static void main(String... args){
-    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
-    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+  public static void main(String... args) {
+    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
 
-    int length = boys.size();
-    if (length != girls.size()) {
+    ArrayList<String> order = new ArrayList<String>();
+
+    int length = girls.size();
+    int length2 = boys.size();
+    for (int i = 0; i < length; i++) {
+      order.add(girls.get(i) + " " + boys.get(i));
     }
-
-    ArrayList<String> order = new ArrayList<String>(length);
-    for (int i = 0; i <= length; i++) {
-      if (i <= girls.size()) {
-        order.add(girls.get(i) + ", " + boys.get(i));
-      }
-      if (i > girls.size()) {
+    if (length < boys.size()) {
+      for (int i = length; i < length2; i++) {
         order.add(boys.get(i));
       }
+      System.out.println(order);
     }
-    System.out.print(order);
   }
 }
