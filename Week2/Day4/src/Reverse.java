8b4513bf19809bed0e2900ace7228e1b33203b11
Nagy Dora <dorinagy06@gmail.com>
2017-03-23 12:39:06 +0100

    correct reverse, forgot the equal sign in loop

--- Week2/Day4/src/Reverse.java
+++ Week2/Day4/src/Reverse.java
@@ -6,7 +6,7 @@ public class Reverse {
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
     System.out.print("");
-    for (int i = reversed.length() - 1; i > 0; i--) {
+    for (int i = reversed.length() -1; i >= 0; i--) {
       System.out.print(reversed.charAt(i));
     }
   }
