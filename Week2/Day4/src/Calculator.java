e5945dfb404b3e1fe09c5d8b98b8b10196fef42b
Nagy Dora <dorinagy06@gmail.com>
2017-03-25 09:25:24 +0100

    use parsInt function in day4 calculator task

--- Week2/Day4/src/Calculator.java
+++ Week2/Day4/src/Calculator.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class Calculator {
   public static void main(String... args) {
 
-    System.out.println("Please type in the expression:");
+    System.out.println("Please type in the expression (eg. + 3 4): ");
     Scanner scanner = new Scanner(System.in);
     String userInput = scanner.nextLine();
 
-    String[] array = userInput.split(" ");
+    String[] inputParts = userInput.split(" ");
 
-    int operand1 = 0;
-    int operand2 = 0;
+    int operand1 = Integer.parseInt(inputParts[2]);
+    int operand2 = Integer.parseInt(inputParts[3]);
 
     if (userInput.contains("+")) {
       System.out.println("The answer is: " + (operand1 + operand2));
