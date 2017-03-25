26956bc832c69bc4679ec6b29e1ef8ecfcdce999
Nagy Dora <dorinagy06@gmail.com>
2017-03-25 09:09:00 +0100

    make day2 square task with equal length sides

--- Week2/Day2/src/Square.java
+++ Week2/Day2/src/Square.java
@@ -9,13 +9,13 @@ public class Square {
     System.out.println("Give me a number! I'll draw a square!");
     int size = input.nextInt();
     for (int i = 1; i <= size; i++) {
-      if (i == 1 || i == size) { //first and last line
+      if (i == 1 || i == size) {      //first and last line
         for (int j = 0; j < size; j++) {
-          System.out.print("%");
+          System.out.print("% ");
         }
       }else {
-        for (int j = 1; j <= size; j++) {
-          if (j == 1 || j == size) {
+        for (int j = 1; j <= size * 2 - 1; j++) {   //left and right side
+          if (j == 1 || j == size * 2 - 1) {
             System.out.print("%");
           }else {
             System.out.print(" ");
