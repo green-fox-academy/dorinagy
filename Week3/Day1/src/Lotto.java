e0e1bedf2055c5cf397cc3772f174b89c2c15a7a
Nagy Dora <dorinagy06@gmail.com>
2017-03-27 15:56:30 +0200

    lotto exercise print out the 5 most frequent numbers

--- Week3/Day1/src/Lotto.java
+++ Week3/Day1/src/Lotto.java
@@ -41,22 +41,100 @@ public class Lotto {
 
       int count = 0;
       Object number = 0;
-      int tempCount = 0;
-      for (int y = 0; y < 5; y++) {
-        for (int x = 0; x < lottoNumbers.size(); x++) {
-          Object tempNumber = lottoNumbers.get(x);
-          for (int z = 0; z < lottoNumbers.size(); z++) {
-            if (lottoNumbers.get(z).equals(tempNumber)) {
-              tempCount = tempCount + 1;
-            }
-            if (tempCount > count) {
-              number = tempNumber;
-              count = tempCount;
-            }
+      for (int y = 0; y < lottoNumbers.size();y++) {
+        Object tempNumber = lottoNumbers.get(y);
+        int tempCount = 0;
+        for (int z = 0; z < lottoNumbers.size(); z++) {
+          if (lottoNumbers.get(z).equals(tempNumber)) {
+            tempCount = tempCount + 1;
+          }
+          if (tempCount > count) {
+            number = tempNumber;
+            count = tempCount;
           }
         }
-        System.out.println(number);
       }
+        System.out.println(number + " " + count);
+
+      ArrayList<Object>numberList = new ArrayList<>(Arrays.asList(number));
+      lottoNumbers.removeAll(numberList);
+
+      int count2 = 0;
+      Object number2 = 0;
+      for (int y = 0; y < lottoNumbers.size();y++) {
+        int tempCount2 = 0;
+        Object tempNumber2 = lottoNumbers.get(y);
+        for (int z = 0; z < lottoNumbers.size(); z++) {
+          if (lottoNumbers.get(z).equals(tempNumber2)) {
+            tempCount2 = tempCount2 + 1;
+          }
+          if (tempCount2 > count2) {
+            number2 = tempNumber2;
+            count2 = tempCount2;
+          }
+        }
+      }
+      System.out.println(number2 + " " + count2);
+
+      ArrayList<Object>numberList2 = new ArrayList<>(Arrays.asList(number2));
+      lottoNumbers.removeAll(numberList2);
+
+      int count3 = 0;
+      Object number3 = 0;
+      for (int y = 0; y < lottoNumbers.size();y++) {
+        int tempCount3 = 0;
+        Object tempNumber3 = lottoNumbers.get(y);
+        for (int z = 0; z < lottoNumbers.size(); z++) {
+          if (lottoNumbers.get(z).equals(tempNumber3)) {
+            tempCount3 = tempCount3 + 1;
+          }
+          if (tempCount3 > count3) {
+            number3 = tempNumber3;
+            count3 = tempCount3;
+          }
+        }
+      }
+      System.out.println(number3 + " " + count3);
+
+      ArrayList<Object>numberList3 = new ArrayList<>(Arrays.asList(number3));
+      lottoNumbers.removeAll(numberList3);
+
+      int count4 = 0;
+      Object number4 = 0;
+      for (int y = 0; y < lottoNumbers.size();y++) {
+        int tempCount4 = 0;
+        Object tempNumber4 = lottoNumbers.get(y);
+        for (int z = 0; z < lottoNumbers.size(); z++) {
+          if (lottoNumbers.get(z).equals(tempNumber4)) {
+            tempCount4 = tempCount4 + 1;
+          }
+          if (tempCount4 > count4) {
+            number4 = tempNumber4;
+            count4 = tempCount4;
+          }
+        }
+      }
+      System.out.println(number4 + " " + count4);
+
+      ArrayList<Object>numberList4 = new ArrayList<>(Arrays.asList(number4));
+      lottoNumbers.removeAll(numberList4);
+
+      int count5 = 0;
+      Object number5 = 0;
+      for (int y = 0; y < lottoNumbers.size();y++) {
+        int tempCount5 = 0;
+        Object tempNumber5 = lottoNumbers.get(y);
+        for (int z = 0; z < lottoNumbers.size(); z++) {
+          if (lottoNumbers.get(z).equals(tempNumber5)) {
+            tempCount5 = tempCount5 + 1;
+          }
+          if (tempCount5 > count5) {
+            number5 = tempNumber5;
+            count5 = tempCount5;
+          }
+        }
+      }
+      System.out.println(number5 + " " + count5);
 
     } catch (Exception ex) {
       System.out.println("Error" + ex.getClass());
