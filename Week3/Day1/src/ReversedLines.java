03ca590caef836fc75d9f11f0482b55d44a0f7f9
Nagy Dora <dorinagy06@gmail.com>
2017-03-27 21:26:27 +0200

    add function to ReversedLines exercise

--- Week3/Day1/src/ReversedLines.java
+++ Week3/Day1/src/ReversedLines.java
@@ -1,7 +1,10 @@
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Nagy Dóra on 2017.03.27..
@@ -10,17 +13,25 @@ public class ReversedLines {
   public static void main(String[] args) {
     try {
       Path filePath = Paths.get("assets/reversed-lines.txt");
-      List<String> lines = Files.readAllLines(filePath);
-      String string = lines.toString();
+      List<String>encryptedLines = Files.readAllLines(filePath);
+      ArrayList<String> decryptedLines = new ArrayList<>();
 
-      String reverse = "";
-      for(int i = string.length() - 1; i >= 0; i--) {
-        reverse = reverse + string.charAt(i);
+      for (String line : encryptedLines) {
+        decryptedLines.add(decryptLines(line));
       }
-      System.out.println(reverse);
+      for (String line : decryptedLines) {
+        System.out.println(line);
+      }
 
     } catch (Exception e) {
       System.out.println("Error " + e.getClass());
     }
+  }
+  public static String decryptLines(String inputLine) {
+    String newLine = new String();
+    for (int i = 1; i <= inputLine.length(); i++) {
+      newLine += inputLine.charAt(inputLine.length() - i);
+    }
+    return newLine;
   }
 }
