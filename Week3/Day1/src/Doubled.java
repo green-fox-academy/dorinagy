31952070dce3126218faeaff86b0c718ff4162e7
Nagy Dora <dorinagy06@gmail.com>
2017-03-27 20:34:23 +0200

    add function to Doubled exercise

--- Week3/Day1/src/Doubled.java
+++ Week3/Day1/src/Doubled.java
@@ -2,6 +2,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Nagy Dóra on 2017.03.27..
@@ -11,9 +12,22 @@ public class Doubled {
     try {
     Path filePath = Paths.get("assets/duplicated-chars.txt");
     List<String> lines = Files.readAllLines(filePath);
+    String string = lines.toString();
+    removeDuplicates(string);
+
     } catch (Exception e) {
       System.out.println("Error" + e.getClass());
     }
-
+  }
+  public static void removeDuplicates (String str) {
+    String removed = "";
+    for (int i = 0; i < str.length(); i++) {
+      if (i == 0) {
+        removed = "" + str.charAt(i);
+      } else if(str.charAt(i - 1) != str.charAt(i)) {
+        removed = removed + str.charAt(i);
+      }
+    }
+    System.out.println(removed);
   }
 }
