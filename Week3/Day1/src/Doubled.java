dba66d60059bad056fd0da8887640944293ea318
Nagy Dora <dorinagy06@gmail.com>
2017-03-27 16:10:26 +0200

    read file into doubled exercise

--- Week3/Day1/src/Doubled.java
+++ Week3/Day1/src/Doubled.java
@@ -1,8 +1,19 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by Nagy Dóra on 2017.03.27..
  */
 public class Doubled {
   public static void main(String[] args) {
-    
+    try {
+    Path filePath = Paths.get("assets/duplicated-chars.txt");
+    List<String> lines = Files.readAllLines(filePath);
+    } catch (Exception e) {
+      System.out.println("Error" + e.getClass());
+    }
+
   }
 }
