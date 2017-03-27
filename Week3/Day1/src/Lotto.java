7b9bbb8c5be6335372dbb85d81f63b935a6b5220
Nagy Dora <dorinagy06@gmail.com>
2017-03-27 12:36:49 +0200

    add sort function to lotto numbers

--- Week3/Day1/src/Lotto.java
+++ Week3/Day1/src/Lotto.java
@@ -3,6 +3,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,16 +17,32 @@ public class Lotto {
 
       List<String>lineParts = new ArrayList<>(Arrays.asList(lines.toString().split("[;,]")));
 
-      //System.out.println(lines.get(1));
+      ArrayList lottoNumbers = new ArrayList();
 
       for (int i = 11; i < lineParts.size(); i += 16) {
-        System.out.println(lineParts.get(i));
+        lottoNumbers.add(lineParts.get(i));
+      }
+      for (int j = 12; j < lineParts.size(); j += 16) {
+        lottoNumbers.add(lineParts.get(j));
+      }
+      for (int k = 13; k < lineParts.size(); k += 16) {
+        lottoNumbers.add(lineParts.get(k));
+      }
+      for (int l = 14; l < lineParts.size(); l += 16) {
+        lottoNumbers.add(lineParts.get(l));
+      }
+      for (int n = 15; n < lineParts.size(); n += 16) {
+        lottoNumbers.add(lineParts.get(n));
       }
 
-      //ArrayList<>
+      Collections.sort(lottoNumbers);
+
+
+      System.out.println(lottoNumbers);
+
 
     } catch (Exception ex) {
       System.out.println("Error" + ex.getClass());
-    }
+      }
   }
 }
