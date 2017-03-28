cf2ef13bef42f1b99b3e4325d3e2b1b33eb9e0d2
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 16:42:36 +0200

    draw lines in quarters in linePlayquarters exercise

--- Week3/Day2/src/LinePlayQuarters.java
+++ Week3/Day2/src/LinePlayQuarters.java
@@ -6,8 +6,40 @@ import java.awt.*;
  */
 public class LinePlayQuarters {
   public static void mainDraw(Graphics graphics){
+    int x1 = 150;
+    int y1 = 0;
+    int x2 = 300;
+    int y2 = 0;
+    for (int i = 0; i < 7.5; i++) {
+      graphics.setColor(Color.CYAN);
+      graphics.drawLine(x1, y1, x2, y2);
+      x1 += 20;
+      y2 += 20;
+    }
 
+    x1 = 0;
+    y1 = 150;
+    x2 = 0;
+    y2 = 300;
 
+    for (int i = 0; i < 7.5; i++) {
+      graphics.setColor(Color.DARK_GRAY);
+      graphics.drawLine(x1, y1, x2, y2);
+      y1 += 20;
+      x2 += 20;
+    }
+
+    x1 = 150;
+    y1 = 150;
+    x2 = 150;
+    y2 = 300;
+
+    for (int i = 0; i < 7.5; i++) {
+      graphics.setColor(Color.DARK_GRAY);
+      graphics.drawLine(x1, y1, x2, y2);
+      y1 += 20;
+      x2 += 20;
+    }
 
   }
   public static void main(String[] args) {
