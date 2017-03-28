9606a4c0cf58e2775c0c76a8d65de0b1bf767f05
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 20:30:37 +0200

    add envelopeStar exercise

---
+++ Week3/Day2/src/EnvelopeStar.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Nagy Dóra on 2017.03.28..
+ */
+public class EnvelopeStar {
+  public static void mainDraw(Graphics graphics){
+    int x1 = 150;
+    int y1 = 0;
+    int x2 = 150;
+    int y2 = 150;
+    for (int i = 0; i < 15; i++) {
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(x1, y1, x2, y2);
+      x2 += 10;
+      y1 += 10;
+    }
+
+    x1 = 150;
+    y1 = 0;
+    x2 = 150;
+    y2 = 150;
+    for (int i = 0; i < 15; i++) {
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(x1, y1, x2, y2);
+      x2 -= 10;
+      y1 += 10;
+    }
+
+    x1 = 300;
+    y1 = 150;
+    x2 = 150;
+    y2 = 150;
+    for (int i = 0; i < 15; i++) {
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(x1, y1, x2, y2);
+      x1 -= 10;
+      y2 += 10;
+    }
+
+    x1 = 0;
+    y1 = 150;
+    x2 = 150;
+    y2 = 150;
+    for (int i = 0; i < 15; i++) {
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(x1, y1, x2, y2);
+      x1 += 10;
+      y2 += 10;
+    }
+
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
