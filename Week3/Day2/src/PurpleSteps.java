21cffa99cf1f6407093f41b1808142d465f73f18
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 12:27:32 +0200

    add purpleSteps exercise

---
+++ Week3/Day2/src/PurpleSteps.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Nagy Dóra on 2017.03.28..
+ */
+public class PurpleSteps {
+  public static void mainDraw(Graphics graphics){
+
+    for (int i = 10; i < 290; i +=10) {
+      int size = 10;
+      graphics.setColor(new Color(153, 58, 164));
+      graphics.fillRect(i, i, size, size);
+    }
+
+  }
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
