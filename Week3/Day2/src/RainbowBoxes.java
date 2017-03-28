504eae9bfedeb0a9bf27a74793317feadf12769b
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 12:51:07 +0200

    add random square to rainbow boxes exercise

--- Week3/Day2/src/RainbowBoxes.java
+++ Week3/Day2/src/RainbowBoxes.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,15 +9,25 @@ import java.util.Scanner;
 public class RainbowBoxes {
   public static void mainDraw(Graphics graphics){
     // create a loop that fills the canvas with rainbow colored squares.
-    System.out.println("Give me the square's size and the color!");
+    System.out.println("Give me the square's size and 3 numbers between 0 and 255 to the color (rgb(0, 145, 173))");
     drawSquares(graphics);
+
+    int x, y;
+    Random r = new Random();
+    x = r.nextInt(640);
+    y = r.nextInt(480);
+    graphics.setColor(Color.GREEN);
+    graphics.fillRect(x, y, 30, 30);
+
   }
 
   public static void drawSquares(Graphics drawSquare) {
     Scanner scanner = new Scanner(System.in);
-    String color = scanner.nextLine();
-    drawSquare.setColor(Color.getColor(color));
     int size = scanner.nextInt();
+    int r = scanner.nextInt();
+    int g = scanner.nextInt();
+    int b = scanner.nextInt();
+    drawSquare.setColor(new Color(r, g, b));
     drawSquare.drawRect((150 - (size / 2)), (150 - (size / 2)), size, size);
   }
 
