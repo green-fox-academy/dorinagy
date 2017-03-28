04203edfe64fdf537c67c747b7666098021f3c26
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 20:32:00 +0200

    connect the dots exercise initial commit

---
+++ Week3/Day2/src/ConnectDots.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+/**
+ * Created by Nagy Dóra on 2017.03.28..
+ */
+public class ConnectDots {
+  public static void mainDraw(Graphics graphics){
+    // create a function that takes 1 parameter:
+    // an array of {x, y} points
+    // and connects them with green lines.
+    // connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+    // connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+    // {120, 100}, {85, 130}, {50, 100}}
+
+
+  }
+  public static void connectDots(Graphics connect) {
+    Scanner scanner = new Scanner(System.in);
+
+
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
