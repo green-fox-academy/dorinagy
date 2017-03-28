8613d48c0d4d7427cfc754f127ea2e5e9c4d72a1
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 16:24:50 +0200

    linePlayQuarters exercise initial commit

---
+++ Week3/Day2/src/LinePlayQuarters.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Nagy Dóra on 2017.03.28..
+ */
+public class LinePlayQuarters {
+  public static void mainDraw(Graphics graphics){
+
+
+
+  }
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
