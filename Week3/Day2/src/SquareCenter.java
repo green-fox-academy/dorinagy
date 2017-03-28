68119373d5f0e6bbce3283c09624bde987e28966
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 11:29:15 +0200

    add square center exercise

---
+++ Week3/Day2/src/SquareCenter.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Nagy Dóra on 2017.03.28..
+ */
+public class SquareCenter {
+  public static void mainDraw(Graphics graphics){
+    // draw a green 10x10 square to the canvas' center.
+    graphics.setColor(Color.GREEN);
+    graphics.drawRect(145, 145,10,10);
+
+  }
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
