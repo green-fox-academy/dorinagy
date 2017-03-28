e172a93ccb73a53690bb17b16aa47bf47f077d90
Nagy Dora <dorinagy06@gmail.com>
2017-03-28 11:25:20 +0200

    add draw horizontal lines exercise

---
+++ Week3/Day2/src/Drawing5.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+/**
+ * Created by Nagy Dóra on 2017.03.28..
+ */
+public class Drawing5 {
+  public static void mainDraw(Graphics graphics){
+
+    System.out.println("Give me two parameters!");
+    drawLines(graphics);
+    System.out.println("Give me two parameters!");
+    drawLines(graphics);
+    System.out.println("Give me two parameters!");
+    drawLines(graphics);
+
+
+  }
+  public static void drawLines(Graphics drawLine) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+    drawLine.setColor(Color.BLUE);
+    drawLine.drawLine(x, y, (x + 50), y);
+  }
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+}
