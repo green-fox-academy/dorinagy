759189ffd17c018169c887282d63ce98aa85e7a9
Nagy Dora <dorinagy06@gmail.com>
2017-03-30 11:31:07 +0200

    add function to strings exercise

--- Week3/Day4/src/Strings.java
+++ Week3/Day4/src/Strings.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by Nagy Dóra on 2017.03.30..
  */
 public class Strings {
+  public static void main(String[] args) {
+    System.out.println("Type a text:  ");
+    Scanner scanner = new Scanner(System.in);
+    String string = scanner.nextLine();
+    System.out.println(replaceChar(string));
+
+  }
+
+  public static String replaceChar(String str) {
+    if (str.contains("x")) {
+      String newStr = str.replace("x", "y");
+      return replaceChar(newStr);
+    } else {
+      return str;
+    }
+  }
 }
