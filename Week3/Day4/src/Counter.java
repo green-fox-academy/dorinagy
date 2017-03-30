85457be3fde24ffd402f4282983dea430a59d086
Nagy Dora <dorinagy06@gmail.com>
2017-03-30 10:08:33 +0200

    add function to counter exercise

--- Week3/Day4/src/Counter.java
+++ Week3/Day4/src/Counter.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by Nagy Dóra on 2017.03.30..
  */
 public class Counter {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Type in a number: ");
+    int number = scanner.nextInt();
+    counter(number);
+  }
+  public static int counter(int n) {
+    if(n == 0) {
+      System.out.println(n);
+      return 0;
+    } else {
+      System.out.println(n);
+      return n + counter(--n);
+    }
+  }
 }
