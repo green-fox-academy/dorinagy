596dc0d67c599b28dcacba3f94b8a91400f2e10a
Nagy Dora <dorinagy06@gmail.com>
2017-03-30 11:02:12 +0200

    add recursive function to bunnies exercise

--- Week3/Day4/src/Bunnies.java
+++ Week3/Day4/src/Bunnies.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by Nagy Dóra on 2017.03.30..
  */
 public class Bunnies {
+  public static void main(String[] args) {
+    System.out.println("Give me the number of bunnies:  ");
+    Scanner scanner = new Scanner(System.in);
+    int bunnies = scanner.nextInt();
+    System.out.println(ears(bunnies));
+
+  }
+
+  public static int ears(int bunny) {
+    if (bunny == 0 ) {
+      return 0;
+    } else {
+      return 2 + ears(--bunny);
+    }
+  }
 }
