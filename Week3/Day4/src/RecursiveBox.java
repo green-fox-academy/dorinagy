ae2989d599aa83f260bc6da945cfffb967764fcf
Nagy Dora <dorinagy06@gmail.com>
2017-03-30 20:21:16 +0200

    add correct fractal function

--- Week3/Day4/src/RecursiveBox.java
+++ Week3/Day4/src/RecursiveBox.java
@@ -6,34 +6,35 @@ import java.awt.*;
  */
 public class RecursiveBox {
   public static void mainDraw(Graphics graphics) {
-    drawBoxes(graphics);
-    int i1 = 600;
-    int n1 = 0;
-    //drawLines(graphics);
+    drawBox(graphics);
+    fractals(graphics, 0, 0, 600);
   }
 
-  public static void drawBoxes(Graphics drawBox) {
+  public static void drawBox(Graphics drawBox) {
     drawBox.setColor(Color.yellow);
     drawBox.fillRect(0, 0, 600, 600);
     drawBox.setColor(Color.black);
     drawBox.drawRect(0, 0, 600, 600);
   }
 
-  public static void drawLines(Graphics drawLine) {
-    drawLine.setColor(Color.black);
-    drawLine.drawLine(200, 0, 200, 600);
-    drawLine.drawLine(400, 0, 400, 600);
-    drawLine.drawLine(0, 200, 600, 200);
-    drawLine.drawLine(0, 400, 600, 400);
-  }
-
-  public static void fractals(Graphics graphics, int i, int n) {
+  public static void drawLine(Graphics graphics, int x, int y, int i) {
     graphics.setColor(Color.black);
-    graphics.drawLine(0, i / 3, i, i / 3);
-    graphics.drawLine(i / 3 * 2, 0, i / 3 * 2, i);
-    graphics.drawLine(i / 3, 0, i / 3, i);
-    graphics.drawLine(0, i / 3 * 2, i, i / 3 * 2);
-   }
+    graphics.drawLine(x + (i / 3), y, x + (i / 3), y + i);
+    graphics.drawLine(x + ((i / 3) * 2), y, x + ((i / 3) * 2), y + i);
+    graphics.drawLine(x, y + (i / 3), x + i, y + (i / 3));
+    graphics.drawLine(x, y + ((i / 3) * 2), x + i, y + ((i / 3) * 2));
+  }
+  public static void fractals(Graphics graphics, int x, int y, int i) {
+    if (i < 1) {
+    return;
+    } else {
+      drawLine(graphics, x, y, i);
+      fractals(graphics, x + (i / 3), y, i / 3);
+      fractals(graphics, x, y + (i / 3), i / 3);
+      fractals(graphics, x + ((i / 3) * 2), y + (i / 3), i / 3);
+      fractals(graphics, x + (i / 3), y + ((i / 3) * 2), i / 3);
+    }
+  }
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
@@ -43,6 +44,7 @@ public class RecursiveBox {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
+
   static class ImagePanel extends JPanel{
     @Override
     protected void paintComponent(Graphics graphics) {
