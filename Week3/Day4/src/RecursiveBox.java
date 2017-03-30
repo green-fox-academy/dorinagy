e53e45d7b08ba36414c11f1bb3191ce82cd277fd
Nagy Dora <dorinagy06@gmail.com>
2017-03-30 14:05:44 +0200

    add function to recursive boxes exercise

--- Week3/Day4/src/RecursiveBox.java
+++ Week3/Day4/src/RecursiveBox.java
@@ -6,24 +6,37 @@ import java.awt.*;
  */
 public class RecursiveBox {
   public static void mainDraw(Graphics graphics) {
-    drawBox(graphics);
-    drawLines(graphics);
-
+    drawBoxes(graphics);
+    int i1 = 600;
+    int n1 = 0;
+    //drawLines(graphics);
   }
 
-  public static void drawBox(Graphics drawBox) {
+  public static void drawBoxes(Graphics drawBox) {
     drawBox.setColor(Color.yellow);
     drawBox.fillRect(0, 0, 600, 600);
     drawBox.setColor(Color.black);
     drawBox.drawRect(0, 0, 600, 600);
   }
 
-  public static void drawLines(Graphics drawLine) {
+    public static void drawLines(Graphics drawLine) {
     drawLine.setColor(Color.black);
     drawLine.drawLine(200, 0, 200, 600);
     drawLine.drawLine(400, 0, 400, 600);
     drawLine.drawLine(0, 200, 600, 200);
     drawLine.drawLine(0, 400, 600, 400);
+  }
+
+  public static void fractals(Graphics graphics, int i, int n) {
+    graphics.setColor(Color.black);
+    if() {}
+      graphics.drawLine(0, i / 3, i, i / 3);
+      graphics.drawLine(i / 3 * 2, 0, i / 3 * 2, i);
+      graphics.drawLine(i / 3, 0, i / 3, i);
+      graphics.drawLine(0, i / 3 * 2, i, i / 3 * 2);
+
+
+    }
   }
 
   public static void main(String[] args) {
