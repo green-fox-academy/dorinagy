1a9f114f7ba83ad95e02aed3ee42753643626bd5
Nagy Dora <dorinagy06@gmail.com>
2017-03-31 13:20:10 +0200

    finished square fractal exercise

--- Week3/Day5/src/SquareFractal.java
+++ Week3/Day5/src/SquareFractal.java
@@ -6,22 +6,42 @@ import java.awt.*;
  */
 public class SquareFractal {
   public static void mainDraw(Graphics graphics) {
-
+    fractals(graphics, 250, 250, 100, 100);
   }
 
-  public static void drawSquare(Graphics square) {
-
+  public static void drawSquare(Graphics square, int x, int y, int l, int h) {
+    square.fillRect(x - l/2, y - h/2, l, h);
   }
 
-  public static void fractals(Graphics graphics) {
-
+  public static void fractals(Graphics graphics, int x, int y, int l, int h) {
+    if (l < 1) {
+      return;
+    } else {
+      drawSquare(graphics, x, y, l, h);
+      graphics.setColor(Color.CYAN);
+      fractals(graphics, x + l, y, l / 3, h / 3);
+      graphics.setColor(Color.RED);
+      fractals(graphics, x - l, y, l / 3, h / 3);
+      graphics.setColor(Color.GREEN);
+      fractals(graphics, x, y + h, l / 3, h / 3);
+      graphics.setColor(Color.MAGENTA);
+      fractals(graphics, x, y - h, l / 3, h / 3);
+      graphics.setColor(Color.ORANGE);
+      fractals(graphics, x + l, y - h, l / 3, h / 3);
+      graphics.setColor(Color.blue);
+      fractals(graphics, x - l, y - h, l / 3, h / 3);
+      graphics.setColor(Color.pink);
+      fractals(graphics, x + l, y + h, l / 3, h / 3);
+      graphics.setColor(Color.yellow);
+      fractals(graphics, x - l, y + h, l / 3, h / 3);
+    }
   }
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(500, 500));
     jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    jFrame.add(new TriangleFractal.ImagePanel());
+    jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
