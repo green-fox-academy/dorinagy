b2e6da202bbec01c249491d7db82db181bc13a55
Nagy Dora <dorinagy06@gmail.com>
2017-04-02 17:02:25 +0200

    add function to connect dots exercise, draw fox

--- Week3/Day2/src/ConnectDots.java
+++ Week3/Day2/src/ConnectDots.java
@@ -7,28 +7,37 @@ import java.util.Scanner;
  */
 public class ConnectDots {
   public static void mainDraw(Graphics graphics){
-    // create a function that takes 1 parameter:
-    // an array of {x, y} points
-    // and connects them with green lines.
-    // connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
-    // connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
-    // {120, 100}, {85, 130}, {50, 100}}
     System.out.println("Type in two points!");
     Scanner scanner = new Scanner(System.in);
     int x = scanner.nextInt();
     int y = scanner.nextInt();
     int[] inputArray = new int[]{x,y};
-    connectDots(inputArray);
-
+    graphics.setColor(Color.GREEN);
+    connectDots(graphics, inputArray);
+    int[][] box = {{10, 10}, {290, 10}, {290, 290}, {10, 290}};
+    int[][] fox = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+    connectDots2(graphics, box);
+    connectDots2(graphics, fox);
   }
-  public static void connectDots(int[] input) {
-    
 
+  public static void connectDots(Graphics graphics, int[] input) {
+    graphics.drawLine(input[0], 0, 0, input[1]);
+  }
+
+  public static void connectDots2(Graphics graphics, int[][] coordinates) {
+    int[] xpoints = new int[coordinates.length];
+    int[] ypoints = new int[coordinates.length];
+    int npoints = coordinates.length;
+    for(int i = 0; i < coordinates.length; i++) {
+      xpoints[i] = coordinates[i][0];
+      ypoints[i] = coordinates[i][1];
+    }
+    graphics.drawPolygon(xpoints, ypoints, npoints);
   }
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setSize(new Dimension(320, 340));
     jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
