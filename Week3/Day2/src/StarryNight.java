06de99ccca45349337264f0ed6b7681b4c34361c
Nagy Dora <dorinagy06@gmail.com>
2017-04-02 18:03:32 +0200

    add random generators to starry night exercise

--- Week3/Day2/src/StarryNight.java
+++ Week3/Day2/src/StarryNight.java
@@ -1,14 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 /**
  * Created by Nagy Dóra on 2017.04.02..
  */
 public class StarryNight {
+  final public static int SCREENSIZE = 300;
+  final public static int STARSIZE = 2;
+  final public static int MINSTARS = 50;
+  final public static int MAXSTARS = 150;
+
   public static void mainDraw(Graphics graphics){
     graphics.setColor(Color.black);
     graphics.fillRect(0, 0, 300, 300);
+    int numberOfStars = numberOfStars(graphics);
+    for (int i = 0; i < numberOfStars; i++) {
+      generateRandomColor(graphics);
+      int x = randomNumber(graphics);
+      int y = randomNumber(graphics);
+      graphics.fillRect(x, y, STARSIZE, STARSIZE);
+    }
 
+  }
+
+  public static int numberOfStars(Graphics stars) {
+    Random randomNrStars = new Random();
+    int star = randomNrStars.nextInt(MAXSTARS - MINSTARS + 1) + MINSTARS;
+    return star;
+  }
+
+  public static int randomNumber(Graphics random) {
+    Random randomNumber = new Random();
+    int r = randomNumber.nextInt(SCREENSIZE - STARSIZE + 1) + STARSIZE;
+    return r;
+  }
+  public static void generateRandomColor(Graphics graphics2) {
+    Random rand = new Random();
+    int n = rand.nextInt(220 - 120 + 1) + 120;
+    graphics2.setColor(new Color(n, n, n));
   }
 
   public static void main(String[] args) {
