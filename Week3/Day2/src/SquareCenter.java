f13e49d7c2cdce8b78e101f773399dec4230ebcd
Nagy Dora <dorinagy06@gmail.com>
2017-04-02 17:14:12 +0200

    reformat code

--- Week3/Day2/src/SquareCenter.java
+++ Week3/Day2/src/SquareCenter.java
@@ -5,26 +5,25 @@ import java.awt.*;
  * Created by Nagy Dóra on 2017.03.28..
  */
 public class SquareCenter {
-  public static void mainDraw(Graphics graphics){
-    // draw a green 10x10 square to the canvas' center.
+  public static void mainDraw(Graphics graphics) {
     graphics.setColor(Color.GREEN);
-    graphics.drawRect(145, 145,10,10);
-
+    graphics.drawRect(145, 145, 10, 10);
   }
+
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setSize(new Dimension(320, 340));
     jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
       mainDraw(graphics);
-
     }
   }
 }
