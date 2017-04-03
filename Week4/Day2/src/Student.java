d9327b6c93c899ddffc9fdd09a667c79a9768771
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 20:28:05 +0200

    add student class hw

---
+++ Week4/Day2/src/Student.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Nagy Dóra on 2017.04.03..
+ */
+public class Student {
+  String name;
+  int age;
+  String gender;
+  String previousOrganization;
+  int skippedDays;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    skippedDays = 0;
+  }
+
+  public Student() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I,m " + name + ", a " + age + " year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+}
