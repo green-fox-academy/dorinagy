acb69f2331df0b377762f37f0a010b6b23a4c38b
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 12:11:02 +0200

    completed fleet of things exercise

--- Week4/Day1/src/FleetOfThings.java
+++ Week4/Day1/src/FleetOfThings.java
@@ -4,11 +4,19 @@
 public class FleetOfThings {
   public static void main(String[] args) {
     Fleet fleet = new Fleet();
-    // Create a fleet of things to have this output:
-    // 1. [ ] Get milk
-    // 2. [ ] Remove the obstacles
-    // 3. [x] Stand up
-    // 4. [x] Eat lunch
+
+    Thing getMilk = new Thing("Get milk");
+    Thing removeobsticles = new Thing("Remove the obstacles");
+    Thing standUp = new Thing("Stand up");
+    Thing eatLunch = new Thing("Eat lunch");
+
+    standUp.complete();
+    eatLunch.complete();
+
+    fleet.add(getMilk);
+    fleet.add(removeobsticles);
+    fleet.add(standUp);
+    fleet.add(eatLunch);
 
     System.out.println(fleet);
   }
