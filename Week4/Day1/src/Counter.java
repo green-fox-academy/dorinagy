7344341e2a8b05cade4e79d13250e56b7db53d0e
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 13:20:09 +0200

    finished counter exercise, add countertest

--- Week4/Day1/src/Counter.java
+++ Week4/Day1/src/Counter.java
@@ -2,5 +2,32 @@
  * Created by Nagy Dóra on 2017.04.03..
  */
 public class Counter {
+  int value;
+  int initvalue;
 
+  public Counter() {
+    value = 0;
+    initvalue = value;
+  }
+
+  public Counter(int value) {
+    this.value = value;
+    initvalue = value;
+  }
+
+  public void add(int number) {
+    value = value + number;
+  }
+
+  public void add() {
+    value += 1;
+  }
+
+  public int get() {
+    return value;
+  }
+
+  public void reset() {
+    value = initvalue;
+  }
 }
