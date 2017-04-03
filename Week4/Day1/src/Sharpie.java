fc3bac19c413e81260aa05c010e13c7ca68d0cb6
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 12:37:17 +0200

    add sharpie exercise

---
+++ Week4/Day1/src/Sharpie.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Nagy Dóra on 2017.04.03..
+ */
+public class Sharpie {
+  String color;
+  float width;
+  float inkAmount;
+
+  @Override
+  public String toString() {
+    return "Sharpie " +
+            "color = '" + color + '\'' +
+            ", width = " + width +
+            ", inkAmount = " + inkAmount;
+  }
+
+  public Sharpie(String color, float width) {
+    this.color = color;
+    this.width = width;
+    inkAmount = 100;
+  }
+
+  public void use() {
+    inkAmount -= 1;
+  }
+
+  public static void main(String[] args) {
+    Sharpie red = new Sharpie("red", 40);
+    System.out.println(red.toString());
+  }
+}
