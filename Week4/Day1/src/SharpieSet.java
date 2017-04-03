4ed18d5facfaa0b9fce7094dcc1cc32aedcdc25f
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 14:09:21 +0200

    initial commit for sharpieset exercise

---
+++ Week4/Day1/src/SharpieSet.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by Nagy Dóra on 2017.04.03..
+ */
+public class SharpieSet {
+  List<Sharpie> sharpieList = new List<Sharpie>();
+
+  public void countUseable() {
+
+  }
+
+  public void removeTrash() {
+
+  }
+}
