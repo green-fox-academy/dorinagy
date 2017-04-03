77a2d2e734b8986cdedec10997d66758c6e77af7
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 13:29:04 +0200

    teacher and students initial commit

---
+++ Week4/Day1/src/Student.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nagy Dóra on 2017.04.03..
+ */
+public class Student {
+  public void learn() {
+  }
+
+  public void question(Teacher answer) {
+  }
+}
---
+++ Week4/Day1/src/Teacher.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Nagy Dóra on 2017.04.03..
+ */
+public class Teacher {
+  public void teach(Student learn) {
+
+  }
+
+  public void answer() {
+  }
+}
