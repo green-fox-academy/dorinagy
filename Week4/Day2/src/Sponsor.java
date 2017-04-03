12ccdae92f3726f2155d3adee25638f053624f93
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 20:29:07 +0200

    add sponsor class hw

---
+++ Week4/Day2/src/Sponsor.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Nagy Dóra on 2017.04.03..
+ */
+public class Sponsor {
+  String name;
+  int age;
+  String gender;
+  String company;
+  int hiredStudents;
+
+  public Sponsor(String name, int age, String gender, String company) {
+    hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    company = "Google";
+    hiredStudents = 0;
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I,m " + name + ", a " + age + " year old " + gender + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+  }
+
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+
+  public void hire() {
+    this.hiredStudents++;
+  }
+}
