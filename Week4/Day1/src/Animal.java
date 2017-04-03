f291cc73d66949772835265fa698939f8567f49e
Nagy Dora <dorinagy06@gmail.com>
2017-04-03 12:34:18 +0200

    reformat animal code

--- Week4/Day1/src/Animal.java
+++ Week4/Day1/src/Animal.java
@@ -11,23 +11,23 @@ public class Animal {
     return name + ":" + " hunger = " + hunger + ", thirst = " + thirst;
   }
 
-  public Animal(String name){
+  public Animal(String name) {
     hunger = 50;
     thirst = 50;
     this.name = name;
   }
 
-  public void eat(){
-    this.hunger -= 1;
+  public void eat() {
+    hunger -= 1;
   }
 
-  public void drink(){
-    this.thirst -= 1;
+  public void drink() {
+    thirst -= 1;
   }
 
-  public void play(){
-    this.thirst -= 1;
-    this.hunger -= 1;
+  public void play() {
+    thirst -= 1;
+    hunger -= 1;
   }
 
   public static void main(String[] args) {
