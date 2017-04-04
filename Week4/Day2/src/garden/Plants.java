bbceb0dea415f8ea6bb3003b5c63b4c0755bd9a5
Nagy Dora <dorinagy06@gmail.com>
2017-04-04 14:21:53 +0200

    add plant class for garden app exercise

---
+++ Week4/Day2/src/garden/Plants.java
@@ -0,0 +1,14 @@
+package garden;
+
+/**
+ * Created by Nagy Dóra on 2017.04.04..
+ */
+public class Plants {
+  double currentWaterAmount;
+  String color;
+
+  public Plants(String color) {
+    this.color = color;
+  }
+
+}
