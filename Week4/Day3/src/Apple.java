24a40f6508200aae2a9ee9c61ea91eb6fdd5ab8b
Nagy Dora <dorinagy06@gmail.com>
2017-04-04 21:26:05 +0200

    add Apple exercise

---
+++ Week4/Day3/src/Apple.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Nagy Dóra on 2017.04.04..
+ */
+public class Apple {
+  private String apple;
+
+  public Apple(String apple) {
+    this.apple = apple;
+  }
+
+  public String getApple() {
+    System.out.println(apple);
+    return apple;
+  }
+}
