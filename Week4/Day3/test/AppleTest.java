af6c6e25c364bcef15c600591f162857ac071eb2
Nagy Dora <dorinagy06@gmail.com>
2017-04-04 21:26:35 +0200

    add apple test class

---
+++ Week4/Day3/test/AppleTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Nagy Dóra on 2017.04.04..
+ */
+class AppleTest {
+  String apple = "apple";
+  Apple Apple = new Apple(apple);
+
+  @Test
+  void getApple() {
+    assertEquals(apple, Apple.getApple());
+  }
+}
