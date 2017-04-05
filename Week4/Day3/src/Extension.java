5e65de4a8101377aff71832c8bdf28dd811ef4ca
Nagy Dora <dorinagy06@gmail.com>
2017-04-05 15:10:41 +0200

    extension exercise fix implementations

--- Week4/Day3/src/Extension.java
+++ Week4/Day3/src/Extension.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -6,18 +8,28 @@ import java.util.List;
  */
 public class Extension {
   int add(int a, int b) {
-    return 5;
+    return a + b;
   }
 
   int maxOfThree(int a, int b, int c) {
-    if (a > b)
+    if (a > b && a > c)
       return a;
-    else
+    else if(b > a && b > c) {
+      return b;
+    } else {
       return c;
+    }
   }
 
   int median(List<Integer> pool) {
-    return pool.get((pool.size()-1)/2);
+    Collections.sort(pool);
+    int median;
+    if (pool.size() % 2 == 0) {
+      median = (pool.get((pool.size() - 1)/2) + pool.get((pool.size() + 1)/2))/2;
+    } else {
+      median = pool.get(pool.size()/2);
+      }
+      return median;
   }
 
   boolean isVowel(char c) {
@@ -30,7 +42,7 @@ public class Extension {
     for (int i = 0; i < length; i++) {
       char c = teve.charAt(i);
       if (isVowel(c)) {
-        teve = String.join(c + "v" + c, teve.split(""+c));
+        teve = String.join(c + "v" + c, teve.split("" + c));
         i+=2;
         length+=2;
       }
--- Week4/Day3/test/ExtensionTest.java
+++ Week4/Day3/test/ExtensionTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -13,6 +11,11 @@ class ExtensionTest {
   @Test
   void testAdd_2and3is5() {
     assertEquals(5, extension.add(2, 3));
+  }
+
+  @Test
+  void testAdd_4and6is10() {
+    assertEquals(10, extension.add(4, 6));
   }
 
   @Test
@@ -31,8 +34,23 @@ class ExtensionTest {
   }
 
   @Test
+  void testMaxOfThree_mine() {
+    assertEquals(5, extension.maxOfThree(3, 5, 4));
+  }
+
+  @Test
   void testMedian_four() {
     assertEquals(5, extension.median(Arrays.asList(7,5,3,5)));
+  }
+
+  @Test
+  void testMedian_mine() {
+    assertEquals(3, extension.median(Arrays.asList(7,3,5,3,3,3)));
+  }
+
+  @Test
+  void testMedian_mine2() {
+    assertEquals(4, extension.median(Arrays.asList(1,2,3,4,6,6,7)));
   }
 
   @Test
@@ -60,4 +78,8 @@ class ExtensionTest {
     assertEquals("lavagovopuvus", extension.translate("lagopus"));
   }
 
+  @Test
+  void testTranslate_mine() {
+    assertEquals("mevegeveszevem", extension.translate("megeszem"));
+  }
 }
