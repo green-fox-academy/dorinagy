40e981ea13511f672ea32016e6d22e7415d8b0c9
Nagy Dora <dorinagy06@gmail.com>
2017-04-05 11:32:26 +0200

    add test to anagram exercise

--- Week4/Day3/test/AnagramTest.java
+++ Week4/Day3/test/AnagramTest.java
@@ -6,11 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by Nagy Dóra on 2017.04.05..
  */
 class AnagramTest {
+  String s1 = "apples";
+  String s2 = "bubble";
   Anagram anagram = new Anagram();
 
   @Test
   void isAnagram() {
-    //assertEquals(anagram.isAnagram(),);
+    assertEquals(anagram.isAnagram(s1, s2), false);
   }
 
 }
