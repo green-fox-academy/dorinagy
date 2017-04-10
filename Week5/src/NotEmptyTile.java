6a814cfc939bd609f6aa3b27ea3f956c732077b2
Nagy Dora <dorinagy06@gmail.com>
2017-04-10 14:38:20 +0200

    not empty class initial commit

---
+++ Week5/src/NotEmptyTile.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Nagy Dóra on 2017.04.10..
+ */
+public class NotEmptyTile extends Tile {
+  public NotEmptyTile(int posX, int posY) {
+    super("assets/wall.png", posX, posY);
+  }
+}
