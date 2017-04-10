1c5091d933c60102336ec2087b204ca29a9288fd
Nagy Dora <dorinagy06@gmail.com>
2017-04-10 16:50:16 +0200

    delete unneeded class, extend game object to jComponent

--- Week5/src/GameObject.java
+++ Week5/src/GameObject.java
@@ -1,10 +1,11 @@
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class GameObject extends Models{
+public class GameObject extends JComponent{
   BufferedImage image;
   int posX, posY;
 
