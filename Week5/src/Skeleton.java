687dd77e9901ebafc3b50d72d86963d67bf650cf
Nagy Dora <dorinagy06@gmail.com>
2017-04-12 10:39:25 +0200

    rpg game skeleton class initial commit

---
+++ Week5/src/Skeleton.java
@@ -0,0 +1,9 @@
+
+/**
+ * Created by Nagy Dóra on 2017.04.12..
+ */
+public class Skeleton extends Monster {
+  public Skeleton(String filename, int posX, int posY) {
+    super("assets/skeleton.png", posX, posY);
+  }
+}
