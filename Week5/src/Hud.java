1dfa6023696e604556a0e03f37cafca1d62e96aa
Nagy Dora <dorinagy06@gmail.com>
2017-04-13 10:19:15 +0200

    rpg - change variable name

--- Week5/src/Hud.java
+++ Week5/src/Hud.java
@@ -4,15 +4,15 @@ import java.awt.*;
  * Created by Nagy Dóra on 2017.04.13..
  */
 public class Hud {
-  String status;
+  String heroStatus;
   Hero hero;
 
   public Hud() {
     hero = new Hero();
-    status = "Hero (Level 1) HP: " + hero.currentHP + "/" + hero.maxHP + " | DP: " + hero.DP + " | SP: " + hero.SP;
+    heroStatus = "Hero (Level 1) HP: " + hero.currentHP + "/" + hero.maxHP + " | DP: " + hero.DP + " | SP: " + hero.SP;
   }
 
   public void draw(Graphics graphics, int x, int y) {
-    graphics.drawString(status, x, y);
+    graphics.drawString(heroStatus, x, y);
   }
 }
