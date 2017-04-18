8e1e81c4242fb219272820158ad80190d10a6f83
Nagy Dora <dorinagy06@gmail.com>
2017-04-18 16:09:35 +0200

    charOperations exercise initial commit

---
+++ Week6/Day1/homework/src/charoperations/CharOperations.java
@@ -0,0 +1,26 @@
+package charoperations;
+
+/**
+ * Created by Nagy Dóra on 2017.04.18..
+ */
+public class CharOperations implements CharSequence {
+
+  @Override
+  public int length() {
+    return 0;
+  }
+
+  @Override
+  public char charAt(int index) {
+    return 0;
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
