8451ce97d332cbd82eb5fba2322be95bd915befd
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 15:52:28 +0200

    sum the array - sum numbers

--- Week6/Day2/src/sumthearray/SumTheArray.java
+++ Week6/Day2/src/sumthearray/SumTheArray.java
@@ -4,12 +4,20 @@ package sumthearray;
  * Created by Nagy Dóra on 2017.04.19..
  */
 public class SumTheArray {
-  
-  public static void main(String[] args) {
 
+  public static void main(String[] args) {
+    Integer[] intArray = {1, 5, 8, 3, 9};
+    Double[] doubleArray = {2.2, 4.4, 3.3, 1.1};
+    sumArray(intArray, 4);
+    sumArray(doubleArray, 3);
   }
 
-  public static <T extends Number> void sumArray(T[] inputArray, int length) {
-
+  public static <T extends Number> double sumArray(T[] inputArray, int length) {
+    Double sum = 0.0;
+    for(int i = 0; i < length; i++) {
+      sum += inputArray[i].doubleValue();
+    }
+    System.out.println(sum);
+    return sum;
   }
 }
