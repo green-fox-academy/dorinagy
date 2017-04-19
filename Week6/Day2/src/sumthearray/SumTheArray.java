24a68d506e1c9affcda36e1906908e1ed0c4dcff
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 15:37:04 +0200

    sum the array exercise initial commit

---
+++ Week6/Day2/src/sumthearray/SumTheArray.java
@@ -0,0 +1,15 @@
+package sumthearray;
+
+/**
+ * Created by Nagy Dóra on 2017.04.19..
+ */
+public class SumTheArray {
+  
+  public static void main(String[] args) {
+
+  }
+
+  public static <T extends Number> void sumArray(T[] inputArray, int length) {
+
+  }
+}
