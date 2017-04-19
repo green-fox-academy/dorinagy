908d833a685ff6c073acb029ed224bcca00f424f
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 15:57:22 +0200

    sum the array - handle exceptions

--- Week6/Day2/src/sumthearray/SumTheArray.java
+++ Week6/Day2/src/sumthearray/SumTheArray.java
@@ -1,5 +1,7 @@
 package sumthearray;
 
+import java.util.Arrays;
+
 /**
  * Created by Nagy Dóra on 2017.04.19..
  */
@@ -8,16 +10,22 @@ public class SumTheArray {
   public static void main(String[] args) {
     Integer[] intArray = {1, 5, 8, 3, 9};
     Double[] doubleArray = {2.2, 4.4, 3.3, 1.1};
-    sumArray(intArray, 4);
-    sumArray(doubleArray, 3);
+    sumArray(intArray, 5);
+    sumArray(doubleArray, 4);
   }
 
   public static <T extends Number> double sumArray(T[] inputArray, int length) {
-    Double sum = 0.0;
-    for(int i = 0; i < length; i++) {
-      sum += inputArray[i].doubleValue();
+    try {
+      Double sum = 0.0;
+      for(int i = 0; i < length; i++) {
+        sum += inputArray[i].doubleValue();
+      }
+      System.out.println(Arrays.toString(inputArray));
+      System.out.println("Sum: " + sum);
+      return sum;
+    } catch(IndexOutOfBoundsException e) {
+      e.printStackTrace();
     }
-    System.out.println(sum);
-    return sum;
+    return 0.0;
   }
 }
