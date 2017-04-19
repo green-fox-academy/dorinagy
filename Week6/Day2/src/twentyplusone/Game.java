56a588665a483f25392fe4853343c866f50e3193
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 12:19:37 +0200

    twenty plus one - add randomNumber method to game class

--- Week6/Day2/src/twentyplusone/Game.java
+++ Week6/Day2/src/twentyplusone/Game.java
@@ -1,7 +1,25 @@
 package twentyplusone;
 
+import java.util.Random;
+
 /**
  * Created by Nagy Dóra on 2017.04.19..
  */
 public class Game {
+  Random random;
+  int randomNumber;
+
+  public Game() {
+    random = new Random();
+  }
+
+  public int generateRandomNum() {
+    randomNumber = random.nextInt(21) + 15;
+    return randomNumber;
+  }
+
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.generateRandomNum();
+  }
 }
