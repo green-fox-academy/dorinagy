a9d056a0a1e9fcfbb610dff18309c56d4d932b74
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 15:04:40 +0200

    splitstring - fix index, print out array

--- Week6/Day2/src/splitstring/SplitString.java
+++ Week6/Day2/src/splitstring/SplitString.java
@@ -1,20 +1,22 @@
 package splitstring;
 
+import java.util.Arrays;
+
 /**
  * Created by Nagy Dóra on 2017.04.19..
  */
 public class SplitString {
 
   public static void main(String[] args) {
-    splitString(4, "lovebarbara");
+    splitString(5, "lovebarbara");
   }
 
   public static String[] splitString(int index, String inputString) {
     try {
       String firstStr = inputString.substring(0, index - 1);
-      String secondStr = inputString.substring(index, inputString.length() - 1);
-      String[] array = {firstStr, secondStr};
-      System.out.println(array);
+      String secondStr = inputString.substring(index - 1, inputString.length());
+      String[] array = new String[] {firstStr, secondStr};
+      System.out.println(Arrays.toString(array));
       return array;
     } catch(IndexOutOfBoundsException e) {
       e.printStackTrace();
