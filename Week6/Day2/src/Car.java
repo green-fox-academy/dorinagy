253c37cd32cd2c959fdce15b38f0106501d22bc2
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 11:17:51 +0200

    parkinglot - fill enums, get random type and color added

--- Week6/Day2/src/Car.java
+++ Week6/Day2/src/Car.java
@@ -1,5 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Nagy Dóra on 2017.04.19..
  */
 public class Car {
+  CarType type;
+  Color color;
+  Random random;
+  List<CarType> carTypeList;
+  List<Color> colorList;
+
+  public Car() {
+    random = new Random();
+    carTypeList = new ArrayList<>(Arrays.asList(CarType.values()));
+    colorList = new ArrayList<>(Arrays.asList(Color.values()));
+    this.type = carTypeList.get(random.nextInt(carTypeList.size()));
+    this.color = colorList.get(random.nextInt(colorList.size()));
+  }
 }
