e71ff5072d9b96999d339829d31a5ba8f506ddf8
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 12:18:41 +0200

    twenty plus one - add methods and constructors to card and deck classes

--- Week6/Day2/src/twentyplusone/Card.java
+++ Week6/Day2/src/twentyplusone/Card.java
@@ -4,4 +4,13 @@ package twentyplusone;
  * Created by Nagy Dóra on 2017.04.19..
  */
 public class Card {
+  String color;
+  String suit;
+  Object rank;
+
+  public Card() {
+    this.color = color;
+    this.suit = suit;
+    this.rank = rank;
+  }
 }
--- Week6/Day2/src/twentyplusone/Deck.java
+++ Week6/Day2/src/twentyplusone/Deck.java
@@ -4,4 +4,24 @@ package twentyplusone;
  * Created by Nagy Dóra on 2017.04.19..
  */
 public class Deck {
+
+  public Deck() {
+
+  }
+
+  public void shuffleDeck() {
+
+  }
+
+  public void pullFirst() {
+
+  }
+
+  public void pullLast() {
+
+  }
+
+  public void pullRandom() {
+
+  }
 }
