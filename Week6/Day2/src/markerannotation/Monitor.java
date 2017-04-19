c3fe88b30576f25d138b9440f40b580cf2f28aac
Nagy Dora <dorinagy06@gmail.com>
2017-04-19 16:47:19 +0200

    add marker annotation exercise

---
+++ Week6/Day2/src/markerannotation/Monitor.java
@@ -0,0 +1,8 @@
+package markerannotation;
+
+/**
+ * Created by Nagy Dóra on 2017.04.19..
+ */
+public @interface Monitor {
+
+}
---
+++ Week6/Day2/src/markerannotation/Screen.java
@@ -0,0 +1,13 @@
+package markerannotation;
+
+/**
+ * Created by Nagy Dóra on 2017.04.19..
+ */
+@Monitor
+public class Screen {
+  int width, height;
+  public Screen(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+}
