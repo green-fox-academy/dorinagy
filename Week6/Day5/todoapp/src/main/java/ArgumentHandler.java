ce0464de3ff7d232baece50282fe198b301b7b1a
Nagy Dora <dorinagy06@gmail.com>
2017-04-21 10:43:33 +0200

    todo app - change argumenthandler, use optionparser

--- Week6/Day5/todoapp/src/main/java/ArgumentHandler.java
+++ Week6/Day5/todoapp/src/main/java/ArgumentHandler.java
@@ -1,37 +1,42 @@
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
 /**
  * Created by Nagy Dóra on 2017.04.21..
  */
 public class ArgumentHandler {
   public void handleArguments(String[] args) {
-    ToDoApp todoapp = new ToDoApp();
     ToDoList todoList = new ToDoList();
-    if (args[0].equals("-l")) {
+    OptionParser parser = new OptionParser();
+    parser.accepts("-l");
+    parser.accepts("-a").withRequiredArg();
+    parser.accepts("-r").withRequiredArg();
+    parser.accepts("-c").withRequiredArg();
+    OptionSet options = parser.parse(args);
+    if(options.has("-l")) {
       todoList.printTasks();
     } else {
       try {
-        if (args[0].equals("-a")) {
-          todoList.addTask(ToDo.UNDONE + args[1]);
-        } else if (args[0].equals("-r")) {
-          todoList.removeTask(Integer.parseInt(args[1]) - 1);
-        } else if (args[0].equals("-c")) {
-          todoList.checkTask(Integer.parseInt(args[1]) - 1);
+        if(options.has("-a")) {
+          todoList.addTask(ToDo.UNDONE + options.valueOf("-a").toString());
+        } else if(options.has("-r")) {
+          todoList.removeTask((int) options.valueOf("-r") - 1);
+        } else if(options.has("-c")) {
+          todoList.checkTask((int) options.valueOf("-c") - 1);
         } else {
           System.out.println("Error: Unsupported argument");
-          todoapp.printUsage();
-          return;
+          PrintUsage.printUsage();
         }
         todoList.saveTasks();
-      } catch (Exception ex) {
-        String err = "Unable to ";
-        err += ((args[0].equals("-a")) ? "add" :
-                (args[0].equals("-r")) ? "remove" :
-                        (args[0].equals("-c")) ? "check" :
-                                "") + ": ";
-        err += (ex instanceof ArrayIndexOutOfBoundsException) ?
-                ((args[0].equals("-a")) ? "no task provided" : "no index provided") :
-                (ex instanceof IndexOutOfBoundsException) ? "index is out of bound" :
-                        (ex instanceof NumberFormatException) ? "index is not a number" :
-                                "";
+      } catch(Exception ex) {
+        String err = "";
+        if(options.has("-a") && options.valueOf("-a").toString().equals("")) {
+          err = "Unable to add, no task provided";
+        } else if(options.has("-r") && options.valueOf("-r").toString().equals("")) {
+          err = "Unable to remove, no index provided";
+        } else if(options.has("-c") && options.valueOf("-c").toString().equals("")) {
+          err = "Unable to check, no index provided";
+        }
         System.out.println(err);
       }
     }
