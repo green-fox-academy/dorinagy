e6e62e8bac9bcdeac539a500a9c5e0cdb1da8e3f
Nagy Dora <dorinagy06@gmail.com>
2017-05-02 11:26:19 +0200

    spring project - add fields, annotations to Greeting class

--- Week8/Day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/Greeting.java
+++ Week8/Day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/Greeting.java
@@ -1,7 +1,15 @@
 package com.greenfoxacademy.springstart;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Created by Nagy Dóra on 2017.05.02..
  */
+@AllArgsConstructor
+@Getter
 public class Greeting {
+  long id;
+  String content;
+
 }
