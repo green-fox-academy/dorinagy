256be2d0a63ac52555f0f6598b8ec61c73b6bd0e
Nagy Dora <dorinagy06@gmail.com>
2017-05-03 15:56:43 +0200

    bankaccount - change values

--- Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/Exercise6.java
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/Exercise6.java
@@ -17,11 +17,11 @@ public class Exercise6 {
   @RequestMapping(value = "/exercise6")
   public String bankAccountList(Model model){
     List<BankAccount> accountList = new ArrayList<>();
-    accountList.add(new BankAccount("Muffin", String.format("%s.00", 4000), "dog"));
-    accountList.add(new BankAccount("Harold", String.format("%s.00", 400), "rabbit"));
-    accountList.add(new BankAccount("Lei", String.format("%s.00", 500), "turtle"));
-    accountList.add(new BankAccount("Mao", String.format("%s.00", 200), "cat"));
-    accountList.add(new BankAccount("Albert", String.format("%s.00", 540), "parrot"));
+    accountList.add(new BankAccount("Zordon", String.format("%s.00", 4000), "lion"));
+    accountList.add(new BankAccount("Mufasa", String.format("%s.00", 400), "lion"));
+    accountList.add(new BankAccount("Pumbaa", String.format("%s.00", 500), "warthog"));
+    accountList.add(new BankAccount("Timon", String.format("%s.00", 200), "meerkat"));
+    accountList.add(new BankAccount("Rafiki", String.format("%s.00", 540), "mandrill"));
     model.addAttribute("accountList", accountList);
     return "exercise6";
   }
