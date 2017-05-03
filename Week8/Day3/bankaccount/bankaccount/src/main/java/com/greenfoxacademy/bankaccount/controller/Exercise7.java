5a8de940cb237d4684cb48b3ddeb59bdabe3f31d
Nagy Dora <dorinagy06@gmail.com>
2017-05-03 15:55:04 +0200

    bankaccount - change values

--- Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/Exercise7.java
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/Exercise7.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.bankaccount.controller;
 
 import com.greenfoxacademy.bankaccount.model.BankAccount;
 import com.greenfoxacademy.bankaccount.model.BankAccount7;
+import com.greenfoxacademy.bankaccount.model.BankAccount8;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,11 @@ public class Exercise7 {
   @RequestMapping(value = "/exercise7")
   public String bankAccountList(Model model){
     List<BankAccount7> accountList = new ArrayList<>();
-    accountList.add(new BankAccount7("Muffin", String.format("%s.00", 4000), "dog", false));
-    accountList.add(new BankAccount7("Harold", String.format("%s.00", 400), "rabbit", false));
-    accountList.add(new BankAccount7("Lei", String.format("%s.00", 500), "turtle", false));
-    accountList.add(new BankAccount7("Mao", String.format("%s.00", 200), "cat", false));
-    accountList.add(new BankAccount7("Albert", String.format("%s.00", 540), "parrot", false));
+    accountList.add(new BankAccount7("Zordon", String.format("%s.00", 4000), "lion", false));
+    accountList.add(new BankAccount7("Mufasa", String.format("%s.00", 400), "lion", true));
+    accountList.add(new BankAccount7("Pumbaa", String.format("%s.00", 500), "warthog", false));
+    accountList.add(new BankAccount7("Timon", String.format("%s.00", 200), "meerkat", false));
+    accountList.add(new BankAccount7("Rafiki", String.format("%s.00", 540), "mandrill", false));
     accountList.add(new BankAccount7("Simba", String.format("%s.00", 300), "lion", true));
     model.addAttribute("accountList", accountList);
     return "exercise7";
