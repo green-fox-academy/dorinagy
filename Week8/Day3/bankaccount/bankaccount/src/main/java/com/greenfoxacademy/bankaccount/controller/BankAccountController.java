2bb242c871703334377382382d67b8576e56310f
Nagy Dora <dorinagy06@gmail.com>
2017-05-03 12:41:22 +0200

    bankaccount - add enjoyController

---
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/BankAccountController.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.bankaccount.controller;
+
+import com.greenfoxacademy.bankaccount.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Created by Nagy Dóra on 2017.05.03..
+ */
+@Controller
+public class BankAccountController {
+
+  @RequestMapping(value = "/exercise1")
+  public String showAccount(Model model) {
+    BankAccount account = new BankAccount("Simba", "Zebra", String.format("%s.00", 2000));
+    model.addAttribute("account", account);
+    return "showFields";
+  }
+}
---
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/EnjoyController.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.bankaccount.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Nagy Dóra on 2017.05.03..
+ */
+@Controller
+public class EnjoyController {
+}
