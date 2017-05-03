adf4a6710774bf4fb94d31a33b7d211ffc5f6bab
Nagy Dora <dorinagy06@gmail.com>
2017-05-03 11:10:58 +0200

    bankAccount exercise initial commit

---
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/model/BankAccount.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.bankaccount.model;
+
+/**
+ * Created by Nagy Dóra on 2017.05.03..
+ */
+public class BankAccount {
+  String name;
+  String balance;
+  String animalType;
+}
