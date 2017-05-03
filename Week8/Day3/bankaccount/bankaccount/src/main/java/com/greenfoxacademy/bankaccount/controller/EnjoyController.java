2ba613c29ba195bcfad03af8f7f00a8d19741e51
Nagy Dora <dorinagy06@gmail.com>
2017-05-03 12:45:43 +0200

    bankaccount - add function to controller

--- Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/EnjoyController.java
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/controller/EnjoyController.java
@@ -1,10 +1,17 @@
 package com.greenfoxacademy.bankaccount.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Nagy Dóra on 2017.05.03..
  */
 @Controller
 public class EnjoyController {
+  @RequestMapping(value = "/exercise4")
+  public String textUtext(Model model) {
+    model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "exercise4";
+  }
 }
