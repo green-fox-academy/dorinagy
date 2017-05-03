478be8567404d65374fd13c323b195fb999c8244
Nagy Dora <dorinagy06@gmail.com>
2017-05-03 12:39:39 +0200

    bankaccount - modify BankAccount class, use getter annotation

--- Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/model/BankAccount.java
+++ Week8/Day3/bankaccount/bankaccount/src/main/java/com/greenfoxacademy/bankaccount/model/BankAccount.java
@@ -1,10 +1,20 @@
 package com.greenfoxacademy.bankaccount.model;
 
+import lombok.Getter;
+
 /**
  * Created by Nagy Dóra on 2017.05.03..
  */
 public class BankAccount {
-  String name;
-  String balance;
-  String animalType;
+
+  @Getter String name;
+  @Getter String balance;
+  @Getter String animalType;
+
+  public BankAccount(String name, String animalType, String balance) {
+    this.name = name;
+    this.balance = balance;
+    this. animalType = animalType;
+  }
+
 }
