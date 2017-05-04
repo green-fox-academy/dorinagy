ab97ee526147cad7f4901aba16788469ba071d4a
Nagy Dora <dorinagy06@gmail.com>
2017-05-04 11:50:15 +0200

    colors - add RedColor class

---
+++ Week8/Day4/day4/day4/src/main/java/com/greenfox/colors/RedColor.java
@@ -0,0 +1,12 @@
+package com.greenfox.colors;
+
+/**
+ * Created by Nagy Dóra on 2017.05.04..
+ */
+public class RedColor implements MyColor{
+
+  @Override
+  public void printColor() {
+    System.out.println("It is red in color...");
+  }
+}
