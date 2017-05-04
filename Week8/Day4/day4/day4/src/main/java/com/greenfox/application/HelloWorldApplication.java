514876973a3cbe336f5f111f5622b198a1139bd3
Nagy Dora <dorinagy06@gmail.com>
2017-05-04 11:33:33 +0200

    move day4 exercises into other project

---
+++ Week8/Day4/day4/day4/src/main/java/com/greenfox/application/HelloWorldApplication.java
@@ -0,0 +1,24 @@
+package com.greenfox.application;
+
+import com.greenfox.HelloWorld;
+import com.greenfox.config.HelloWorldConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created by Nagy Dóra on 2017.05.04..
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloWorldApplication.class, args);
+
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+
+    HelloWorld helloWorld = ctx.getBean(HelloWorld.class);
+    helloWorld.setMessage("Hello World!");
+    helloWorld.getMessage();
+  }
+}
---
+++ Week8/Day4/day4/day4/src/main/java/com/greenfox/config/HelloWorldConfig.java
@@ -0,0 +1,10 @@
+package com.greenfox.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Nagy Dóra on 2017.05.04..
+ */
+@Configuration
+public class HelloWorldConfig {
+}
