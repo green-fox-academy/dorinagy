35af89f86dff444ae6496f644808fe23a6e814a6
Nagy Dora <dorinagy06@gmail.com>
2017-05-04 16:29:17 +0200

    TTC - static page added, set up project

---
+++ Week8/Project/foxclub/foxclub/src/main/java/com/greenfox/foxclub/FoxClubController.java
@@ -0,0 +1,11 @@
+package com.greenfox.foxclub;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Nagy Dóra on 2017.05.04..
+ */
+@Controller
+public class FoxClubController {
+
+}
--- Week8/Project/foxclub/foxclub/src/main/java/com/greenfox/foxclub/FoxclubApplication.java
+++ Week8/Project/foxclub/foxclub/src/main/java/com/greenfox/foxclub/FoxclubApplication.java
@@ -2,9 +2,15 @@ package com.greenfox.foxclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class FoxclubApplication {
+
+	@RequestMapping(value = "")
+	public String showStaticPage() {
+		return "index.html";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoxclubApplication.class, args);
