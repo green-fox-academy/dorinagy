e000b7347957202ed7ce7bde2286a03832e80c9d
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 12:04:44 +0200

    todo - change back mapping to original in controller

--- Week8/Day2/todo/todo/src/main/java/com/greenfox/controller/TodoController.java
+++ Week8/Day2/todo/todo/src/main/java/com/greenfox/controller/TodoController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Nagy Dóra on 2017.05.09..
@@ -18,9 +17,8 @@ public class TodoController {
   TodoRepository todoRepo;
 
   @RequestMapping(value = {"/", "/list"})
-  public ModelAndView list(Model model) {
-    ModelAndView m = new ModelAndView();
-    m.setViewName("todo");
-  return m;
+  public String list(Model model) {
+    model.addAttribute("todo");
+  return "todo";
   }
 }
