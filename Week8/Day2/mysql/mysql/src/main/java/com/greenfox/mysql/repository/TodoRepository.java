330812ec88bff9edc1c01013604d7c701b4a4873
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 16:12:53 +0200

    sql2 - add list of active and inactive todos to repository

--- Week8/Day2/mysql/mysql/src/main/java/com/greenfox/mysql/repository/TodoRepository.java
+++ Week8/Day2/mysql/mysql/src/main/java/com/greenfox/mysql/repository/TodoRepository.java
@@ -4,9 +4,16 @@ import com.greenfox.mysql.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * Created by Nagy Dóra on 2017.05.09..
  */
 @Component
 public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+  List<Todo> findByIsDoneTrue();
+
+  List<Todo> findByIsDoneFalse();
+
 }
