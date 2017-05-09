89f7171dae240d9b190d7294c849b0260d305f73
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 08:58:40 +0200

    todo - add todo class

--- Week8/Day2/todo/todo/src/main/java/com/greenfox/todo/Todo.java
+++ Week8/Day2/todo/todo/src/main/java/com/greenfox/todo/Todo.java
@@ -4,4 +4,40 @@ package com.greenfox.todo;
  * Created by Nagy Dóra on 2017.05.09..
  */
 public class Todo {
+  private int id;
+  private String title;
+  private boolean isUrgent;
+  private boolean isDone;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return isUrgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    isUrgent = urgent;
+  }
+
+  public boolean isDone() {
+    return isDone;
+  }
+
+  public void setDone(boolean done) {
+    isDone = done;
+  }
 }
