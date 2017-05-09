e96ad8e719073ccdf3441be2953787c7e878d052
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 11:51:41 +0200

    todo - add component annotation to repository

--- Week8/Day2/todo/todo/src/main/java/com/greenfox/repository/TodoRepository.java
+++ Week8/Day2/todo/todo/src/main/java/com/greenfox/repository/TodoRepository.java
@@ -1,7 +1,13 @@
-package com.greenfox;
+package com.greenfox.repository;
+
+import com.greenfox.todo.Todo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Nagy Dóra on 2017.05.09..
  */
-public class TodoRepository {
+@Component
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
 }
