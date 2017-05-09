8d33d890329cb645cfa456b38e2560180204f5b2
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 08:59:02 +0200

    todo - add todo controller

--- Week8/Day2/todo/todo/src/main/java/com/greenfox/todo/controller/TodoController.java
+++ Week8/Day2/todo/todo/src/main/java/com/greenfox/todo/controller/TodoController.java
@@ -1,7 +1,19 @@
 package com.greenfox.todo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Nagy Dóra on 2017.05.09..
  */
+@Controller
 public class TodoController {
+
+  @RequestMapping(value = "/list")
+  @ResponseBody
+  public String list(Model model) {
+  return "This is my first todo.";
+  }
 }
