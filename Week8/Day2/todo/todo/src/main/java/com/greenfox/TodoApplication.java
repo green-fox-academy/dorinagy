e1b44643d98a7896adbe9d0470bf3b18ba63ac7d
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 12:01:17 +0200

    todo - add run method to app class

--- Week8/Day2/todo/todo/src/main/java/com/greenfox/TodoApplication.java
+++ Week8/Day2/todo/todo/src/main/java/com/greenfox/TodoApplication.java
@@ -1,12 +1,24 @@
 package com.greenfox;
 
+import com.greenfox.repository.TodoRepository;
+import com.greenfox.todo.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner{
+
+	@Autowired
+	TodoRepository todoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		todoRepo.save(new Todo("I have to learn Object Relational Mapping"));
 	}
 }
