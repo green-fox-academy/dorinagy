ba125ef702b1a0a4633f92df7f47f15cbd922532
Nagy Dora <dorinagy06@gmail.com>
2017-05-09 16:12:07 +0200

    sql2 - map to active todos added

--- Week8/Day2/mysql/mysql/src/main/java/com/greenfox/mysql/controller/TodoController.java
+++ Week8/Day2/mysql/mysql/src/main/java/com/greenfox/mysql/controller/TodoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Nagy Dóra on 2017.05.09..
@@ -19,6 +20,16 @@ public class TodoController {
   @RequestMapping(value = {"/", "/list"})
   public String list(Model model) {
     model.addAttribute("todos", todoRepo.findAll());
+    return "todo";
+  }
+
+  @RequestMapping(value = "/list/")
+  public String active(@RequestParam(value = "isActive", required = false) boolean isActive, Model model) {
+    if (isActive) {
+      model.addAttribute("todos", todoRepo.findByIsDoneFalse());
+    } else {
+      model.addAttribute("todos", todoRepo.findAll());
+    }
     return "todo";
   }
 }
