c6b62b095bb272f91cec23f5ccc73106c0385694
Nagy Dora <dorinagy06@gmail.com>
2017-05-10 11:48:25 +0200

    w8d3 - simplify controller

--- Week8/Day3/rest/rest/src/main/java/com/greenfox/controller/Controller.java
+++ Week8/Day3/rest/rest/src/main/java/com/greenfox/controller/Controller.java
@@ -13,15 +13,12 @@ import org.springframework.web.bind.annotation.*;
 public class Controller {
 
   @ExceptionHandler(MissingServletRequestParameterException.class)
-  @ResponseBody
   public String handleMissingInput() {
     return "error: " + "Please provide an input!";
   }
 
   @RequestMapping(value = "/doubling", method = RequestMethod.GET)
-  @ResponseBody
-  public Doubling doubling(@RequestParam(value = "input") int input) {
-    Doubling output = new Doubling(input, input * 2);
-    return output;
+  public Doubling doubling(@RequestParam(value = "input", required = true) int input) {
+    return new Doubling(input, input * 2);
   }
 }
