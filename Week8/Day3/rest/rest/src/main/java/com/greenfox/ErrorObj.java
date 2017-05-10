cfd03e45747e2799f80e309790dcda4258a63bf8
Nagy Dora <dorinagy06@gmail.com>
2017-05-10 13:35:58 +0200

    w8d3 - add errorobj class

--- Week8/Day3/rest/rest/src/main/java/com/greenfox/ErrorObj.java
+++ Week8/Day3/rest/rest/src/main/java/com/greenfox/ErrorObj.java
@@ -3,5 +3,15 @@ package com.greenfox;
 /**
  * Created by Nagy Dóra on 2017.05.10..
  */
-public class ErrorObj {
+public class ErrorObj implements ResponseObject {
+
+  private String error;
+
+  public ErrorObj(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
 }
