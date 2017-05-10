0d5533b24067a070dbaefcb1081b636915c3815d
Nagy Dora <dorinagy06@gmail.com>
2017-05-10 11:26:13 +0200

    w8d3 - add doubling class

---
+++ Week8/Day3/rest/rest/src/main/java/com/greenfox/Doubling.java
@@ -0,0 +1,23 @@
+package com.greenfox;
+
+/**
+ * Created by Nagy Dóra on 2017.05.10..
+ */
+public class Doubling {
+
+  private int received;
+  private int result;
+
+  public Doubling(int received, int result) {
+    this.received = received;
+    this.result = result;
+  }
+
+  public int getReceived() {
+    return received;
+  }
+
+  public int getResult() {
+    return result;
+  }
+}
