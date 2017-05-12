dd6283c5700a764e26f336c491a094d867feef9c
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 11:03:39 +0200

    reddit - delete unnecessary imports

--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsRestController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsRestController.java
@@ -1,14 +1,10 @@
 package com.greenfox.dorinagy.reddit.controller;
 
-import com.greenfox.dorinagy.reddit.model.Post;
-import com.greenfox.dorinagy.reddit.service.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;im
+import com.greenfox.dorinagy.reddit.service.PostRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by Nagy Dóra on 2017.05.12..
