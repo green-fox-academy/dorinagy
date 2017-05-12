6f71f46457df46f5ecdeabdfa5611b17f578d775
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 10:21:44 +0200

    reddit - add controllers

--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/RedditController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/RedditController.java
@@ -1,7 +1,10 @@
-package com.greenfox.dorinagy.reddit;
+package com.greenfox.dorinagy.reddit.controller;
+
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
+@Controller
 public class RedditController {
 }
--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/RedditRestController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/RedditRestController.java
@@ -1,7 +1,10 @@
-package com.greenfox.dorinagy.reddit;
+package com.greenfox.dorinagy.reddit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
+@RestController
 public class RedditRestController {
 }
