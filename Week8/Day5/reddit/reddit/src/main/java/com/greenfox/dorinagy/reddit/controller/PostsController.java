93f349917d0a29b7e1b30af3e0b8800af8f8a922
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 12:49:34 +0200

    reddit - add crossorigin annotation to controller

--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsController.java
@@ -3,10 +3,13 @@ package com.greenfox.dorinagy.reddit.controller;
 import com.greenfox.dorinagy.reddit.service.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
 @Controller
+@CrossOrigin("*")
 public class PostsController {
 
   @Autowired
