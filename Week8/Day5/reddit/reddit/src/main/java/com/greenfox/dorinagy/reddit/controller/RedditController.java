be4c1f9353c37a6ee81e9f36b10ab54361fae917
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 10:21:23 +0200

    reddit - add getters and setters to post class

---
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/RedditController.java
@@ -0,0 +1,7 @@
+package com.greenfox.dorinagy.reddit;
+
+/**
+ * Created by Nagy Dóra on 2017.05.12..
+ */
+public class RedditController {
+}
--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/model/Post.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/model/Post.java
@@ -1,14 +1,68 @@
 package com.greenfox.dorinagy.reddit.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
+@Entity
 public class Post {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private String title;
   private String href;
   private Timestamp timestamp;
   private long score;
+
+  public Post() {
+  }
+
+  public Post(long id, String title, String href, Timestamp timestamp, long score) {
+    this.id  =id;
+    this.title = title;
+    this.href = href;
+    this.timestamp = timestamp;
+    this.score = score;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getHref() {
+    return href;
+  }
+
+  public void setHref(String href) {
+    this.href = href;
+  }
+
+  public Timestamp getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public long getScore() {
+    return score;
+  }
+
+  public void setScore(long score) {
+    this.score = score;
+  }
 }
