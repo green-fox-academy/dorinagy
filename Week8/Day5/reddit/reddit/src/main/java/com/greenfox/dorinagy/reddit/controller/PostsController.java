5f2b27a77d0e877ac4167733d9a95c580657920f
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 11:02:38 +0200

    reddit - add posts root

--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsController.java
@@ -3,7 +3,6 @@ package com.greenfox.dorinagy.reddit.controller;
 import com.greenfox.dorinagy.reddit.service.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
@@ -12,4 +11,5 @@ public class PostsController {
 
   @Autowired
   PostRepository postRepository;
+
 }
--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsRestController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsRestController.java
@@ -1,10 +1,26 @@
 package com.greenfox.dorinagy.reddit.controller;
 
+import com.greenfox.dorinagy.reddit.model.Post;
+import com.greenfox.dorinagy.reddit.service.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
 @RestController
 public class PostsRestController {
+
+  @Autowired
+  PostRepository postRepository;
+
+  @RequestMapping(value = "/posts", method = RequestMethod.GET)
+  public Iterable listPosts() {
+    return postRepository.findAll();
+  }
 }
