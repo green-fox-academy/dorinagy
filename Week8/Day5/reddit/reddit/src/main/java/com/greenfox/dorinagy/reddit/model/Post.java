998a19b90a0781c06397ca3d1c1bdde4c6b23287
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 10:14:37 +0200

    reddit - add Post class

---
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/model/Post.java
@@ -0,0 +1,14 @@
+package com.greenfox.dorinagy.reddit.model;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Nagy Dóra on 2017.05.12..
+ */
+public class Post {
+  private long id;
+  private String title;
+  private String href;
+  private Timestamp timestamp;
+  private long score;
+}
