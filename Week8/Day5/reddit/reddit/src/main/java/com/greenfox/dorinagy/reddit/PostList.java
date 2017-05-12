a913747f74df1c29187d00e3fb25971cc75bc3ca
Nagy Dora <dorinagy06@gmail.com>
2017-05-12 12:29:08 +0200

    reddit - add post list class

--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/PostList.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/PostList.java
@@ -1,7 +1,22 @@
 package com.greenfox.dorinagy.reddit;
 
+import com.greenfox.dorinagy.reddit.model.Post;
+import com.greenfox.dorinagy.reddit.service.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * Created by Nagy Dóra on 2017.05.12..
  */
 public class PostList {
+
+  private Iterable<Post> posts;
+
+  @Autowired
+  PostRepository postRepository;
+
+  public void preparePostList() {
+    posts = postRepository.findAll();
+  }
 }
