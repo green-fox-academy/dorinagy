2cd3b64aaab70a836750f11c444146b944b06c4b
Nagy Dora <dorinagy06@gmail.com>
2017-05-13 09:45:49 +0200

    reddit - use thymeleaf, show posts on the page

--- Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsController.java
+++ Week8/Day5/reddit/reddit/src/main/java/com/greenfox/dorinagy/reddit/controller/PostsController.java
@@ -3,6 +3,7 @@ package com.greenfox.dorinagy.reddit.controller;
 import com.greenfox.dorinagy.reddit.service.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,7 +16,8 @@ public class PostsController {
   PostRepository postRepository;
 
   @RequestMapping(value = "/")
-  public String home() {
+  public String home(Model model) {
+    model.addAttribute("posts", postRepository.findAll());
     return "index";
   }
 }
