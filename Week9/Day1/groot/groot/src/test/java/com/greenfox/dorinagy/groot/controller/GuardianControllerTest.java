5166591fc8027b6f207ba64bda6c4ad8a3598f33
Nagy Dora <dorinagy06@gmail.com>
2017-05-15 12:08:17 +0200

    groot - add test methods, fix to run tests

--- Week9/Day1/groot/groot/src/test/java/com/greenfox/dorinagy/groot/controller/GuardianControllerTest.java
+++ Week9/Day1/groot/groot/src/test/java/com/greenfox/dorinagy/groot/controller/GuardianControllerTest.java
@@ -1,34 +1,8 @@
-package com.greenfox.dorinagy.groot.controller;
-
-import com.greenfox.dorinagy.groot.GrootApplication;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by Nagy Dóra on 2017.05.15..
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = GrootApplication.class)
-@WebAppConfiguration
-@EnableWebMvc
 class GuardianControllerTest {
 
-  private MockMvc mockMvc;
-
-  @Autowired
-  private WebApplicationContext webApplicationContext;
-
-  @Before
-  public void setup() throws Exception {
-    this.mockMvc = webAppContextSetup(webApplicationContext).build();
-  }
 }
