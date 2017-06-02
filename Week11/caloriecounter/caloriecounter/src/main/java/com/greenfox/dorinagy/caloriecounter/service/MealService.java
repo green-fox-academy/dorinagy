0d3aa1fe34916ec6c1f30d4e3c4b51f3f1403b53
Nagy Dora <dorinagy06@gmail.com>
2017-06-02 15:20:51 +0200

    practise exercise - add service class

---
+++ Week11/caloriecounter/caloriecounter/src/main/java/com/greenfox/dorinagy/caloriecounter/service/MealService.java
@@ -0,0 +1,11 @@
+package com.greenfox.dorinagy.caloriecounter.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Nagy Dóra on 2017.06.02..
+ */
+@Service
+public class MealService {
+  
+}
