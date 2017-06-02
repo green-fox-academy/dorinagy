e9f2cdca487c4bb3257f3f81ec667f5246852864
Nagy Dora <dorinagy06@gmail.com>
2017-06-02 15:27:39 +0200

    practise exercise - add sum calories and meals method

--- Week11/caloriecounter/caloriecounter/src/main/java/com/greenfox/dorinagy/caloriecounter/service/MealService.java
+++ Week11/caloriecounter/caloriecounter/src/main/java/com/greenfox/dorinagy/caloriecounter/service/MealService.java
@@ -1,5 +1,9 @@
 package com.greenfox.dorinagy.caloriecounter.service;
 
+import com.greenfox.dorinagy.caloriecounter.model.Meal;
+import com.greenfox.dorinagy.caloriecounter.repository.MealRepository;
+import com.greenfox.dorinagy.caloriecounter.repository.MealTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,5 +11,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MealService {
-  
+
+  @Autowired
+  MealRepository mealRepository;
+
+  @Autowired
+  MealTypeRepository mealTypeRepository;
+
+  public long sumMeals() {
+    return mealRepository.count();
+  }
+
+  public int sumCalories() {
+    int sumCalories = 0;
+    Iterable<Meal> meals = mealRepository.findAll();
+    for(Meal meal : meals) {
+      sumCalories += meal.getCalories();
+    }
+    return sumCalories;
+  }
 }
