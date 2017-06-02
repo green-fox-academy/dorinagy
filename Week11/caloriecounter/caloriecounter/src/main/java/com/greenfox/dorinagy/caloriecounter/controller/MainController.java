156ec7a34b8d903751f49c71742bd755ce050243
Nagy Dora <dorinagy06@gmail.com>
2017-06-02 15:51:51 +0200

    practise exercise - add postmapping endpoint

--- Week11/caloriecounter/caloriecounter/src/main/java/com/greenfox/dorinagy/caloriecounter/controller/MainController.java
+++ Week11/caloriecounter/caloriecounter/src/main/java/com/greenfox/dorinagy/caloriecounter/controller/MainController.java
@@ -1,11 +1,14 @@
 package com.greenfox.dorinagy.caloriecounter.controller;
 
+import com.greenfox.dorinagy.caloriecounter.model.Meal;
 import com.greenfox.dorinagy.caloriecounter.repository.MealRepository;
 import com.greenfox.dorinagy.caloriecounter.repository.MealTypeRepository;
+import com.greenfox.dorinagy.caloriecounter.service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Nagy Dóra on 2017.06.01..
@@ -19,8 +22,18 @@ public class MainController {
   @Autowired
   MealTypeRepository mealTypeRepository;
 
+  @Autowired
+  MealService mealService;
+
   @GetMapping(value = "/")
   public String homePage(Model model) {
+    model.addAttribute("meals", mealRepository.findAll());
+    return "index";
+  }
+
+  @PostMapping(value = "/")
+  public String setCalorieTable(Model model, Meal meal) {
+    mealService.addNewMeal(meal);
     model.addAttribute("meals", mealRepository.findAll());
     return "index";
   }
