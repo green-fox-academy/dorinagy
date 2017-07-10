4dbf5bce0b179eb672506b54b666a38370e33fd6
Nagy Dora <dorinagy06@gmail.com>
2017-07-10 10:12:01 +0200

    rename function

--- final-exam-exercise/exercise/src/Main.java
+++ final-exam-exercise/exercise/src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
   public static void main(String[] args) {
     int[] inputArray = {1, 3, 8, 6, 7, 4};
-    getBiggestOfSecondElements(inputArray);
+    getMaxOfSecondElements(inputArray);
   }
 
-  public static int getBiggestOfSecondElements(int[] intArray) {
+  public static int getMaxOfSecondElements(int[] intArray) {
     int max = 0;
     for (int i = 1; i <= intArray.length; i += 2) {
       if (intArray[i] > max) {
