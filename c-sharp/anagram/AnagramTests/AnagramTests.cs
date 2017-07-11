6c7b131ab0691f6746f15397c85540af5bf8d5f9
Nagy Dora <dorinagy06@gmail.com>
2017-07-11 11:46:26 +0200

    add anagram exercise

---
+++ c-sharp/anagram/AnagramTests/AnagramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram.Tests
+{
+    [TestClass()]
+    public class AnagramTests
+    {
+        [TestMethod()]
+        public void IsAnagramTest()
+        {
+           Assert.AreEqual(true, Anagram.IsAnagram("alma", "aalm"));
+        }
+    }
+}
---
+++ c-sharp/anagram/anagram/Anagram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class Anagram
+    {
+        public static bool IsAnagram(string string1, string string2)
+        {
+
+            if (string.IsNullOrEmpty(string1) || string.IsNullOrEmpty(string2))
+                return false;
+            if (string1.Length != string2.Length)
+                return false;
+
+            String.Concat(string1.OrderBy(c => c));
+            String.Concat(string2.OrderBy(c => c));
+
+            char[] charArray1 = string1.ToLower().ToCharArray();
+            char[] charArray2 = string2.ToLower().ToCharArray();
+
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+
+            string sortedString1 = new string(charArray1);
+            string sortedString2 = new string(charArray2);
+
+            if (sortedString1 == sortedString2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
